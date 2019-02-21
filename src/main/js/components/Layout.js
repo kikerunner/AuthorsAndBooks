95a34d6501b0eda0ad36ccaa2a673dfab1310e48
Enrique Garcia <antonio.garcia.gallur@gmail.com>
2019-02-21 10:59:53 +0100

    Signed-off-by: Enrique Garcia <antonio.garcia.gallur@gmail.com>
    Trabajando en las rutas
    
    Signed-off-by: Enrique Garcia <antonio.garcia.gallur@gmail.com>

--- src/main/js/components/Layout.js
+++ src/main/js/components/Layout.js
@@ -1,13 +1,29 @@
 import React from "react"
 import AddAccount from "./AddAccount"
+import UserList from "./UserList"
+import { NavLink, Switch } from 'react-router-dom';
 
 export default class Layout extends React.Component {
 
   render() {
    
     return (
-      <div class="text-center">
-        <AddAccount />
+      <BrowserRouter>
+      <div className="App">
+        <div className="nav">
+            <NavLink exact to="/AddAccount" activateClassName="active">AddAcount</NavLink>
+            <NavLink to="/UserList" activateClassName="activate">Lista de Users</NavLink>
+        </div>
+        <Switch>
+            <route exact path="/AddAccount" component={AddAccount} />
+            <route path="/UserList" component={UserList} />
+        </Switch>
+      
+        <div className="text-center">
+          <AddAccount />
+          <UserList />
+        </div>
+      </BrowserRouter>
       </div>
     )
   }
